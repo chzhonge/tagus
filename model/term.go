dbb1df73c018b3560d88bf023e9bd20d651e0f59
chzhonge <chzhonge@gmail.com>
2022-02-09 20:14:15 +0800

    fix: fix id auto incr not working
    refs https://blog.csdn.net/duxing_langzi/article/details/115521414

--- model/term.go
+++ model/term.go
@@ -3,7 +3,7 @@ package model
 import "gorm.io/gorm"
 
 type Term struct {
-	ID      uint   `gorm:"primaryKey;column:id;type:int(10);unsigned;"`
+	ID      uint   `gorm:"column:id;unsigned;autoIncrement;"`
 	UserID  uint   `gorm:"column:user_id;type:int(10);unsigned;index;"`
 	Name    string `gorm:"column:name;type:varchar(60);not null;index;"`
 	Mapping string `gorm:"column:mapping;type:text;not null"`
--- model/user.go
+++ model/user.go
@@ -3,7 +3,7 @@ package model
 import "gorm.io/gorm"
 
 type User struct {
-	ID          uint   `gorm:"primaryKey;column:id;type:int(10);unsigned;"`
+	ID          uint   `gorm:"column:id;unsigned;autoIncrement;"`
 	UserName    string `gorm:"column:username;type:varchar(50);unique;index;not null;"`
 	Password    string `gorm:"column:password;type:char(60);not null;"`
 	DisplayName string `gorm:"column:displayname;type:varchar(20);not null;"`
