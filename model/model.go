b92dc94e12f7bf2f16f73ef0e5fcf709319b95a8
chzhonge <chzhonge@gmail.com>
2022-02-19 11:39:24 +0800

    feat: enable sql logger

--- model/model.go
+++ model/model.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"tagus/config"
 )
 
@@ -11,7 +12,11 @@ var DBConn *gorm.DB
 
 func Init() {
 	var err error
-	DBConn, err = gorm.Open(sqlite.Open(config.DbConfig.GetConnStr()), &gorm.Config{})
+
+	DBConn, err = gorm.Open(
+		sqlite.Open(config.DbConfig.GetConnStr()),
+		&gorm.Config{Logger: logger.Default.LogMode(logger.Info)},
+	)
 	if err != nil {
 		panic(fmt.Errorf("Fatal error DBConn : %v ", err))
 	}
